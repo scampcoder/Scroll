b903ad49e441278e115cc66ce752c28030778191
Victoria Kee <victoriakee95@gmail.com>
2021-04-29 13:31:09 -0500

    Show toTop btn

--- backend/app.js
+++ backend/app.js
@@ -32,6 +32,11 @@ window.addEventListener('scroll', function(){
     } else {
         navbar.classList.remove('fixed-nav');
     }
+    if(scrollHeight > 500){
+        topLink.classList.add('show-link');
+    } else {
+        topLink.classList.remove('show-link');
+    }
 });
 // ********** smooth scroll ************
 // select links
