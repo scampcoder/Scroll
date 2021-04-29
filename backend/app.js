4eec2af04a237705f4a587bd5732e47324c02efb
Victoria Kee <victoriakee95@gmail.com>
2021-04-29 14:05:38 -0500

    Close navbar after selection

--- backend/app.js
+++ backend/app.js
@@ -40,3 +40,21 @@ window.addEventListener('scroll', function(){
 });
 // ********** smooth scroll ************
 // select links
+const scrollLinks = document.querySelectorAll('.scroll-link');
+scrollLinks.forEach(function(link){
+    link.addEventListener('click', function(e){
+        //prevent default
+        e.preventDefault();
+        //navigate to specific spot
+        const id = e.currentTarget.getAttribute('href').slice(1);
+        //console.log(id);
+        const element = document.getElementById(id);
+        let position = element.offsetTop;
+        //console.log(position);
+        window.scrollTo({
+            left: 0,
+            top: position
+        });
+        linksContainer.style.height = 0;
+    });
+});
